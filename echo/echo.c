fe8020b1d3b761e2166c28b1f51f39a1efe74ebd
Nicolo <nicolovizzini2001@gmail.com>
2025-11-27 22:07:52 +0100

    Added -n flag

--- echo/echo.c
+++ echo/echo.c
@@ -1,15 +1,33 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
-void echo (){
-    return;
+int check_flags (int argc, char **argv, int *i){
+    //printf("inside check flags\n");
+    while(*i<argc){ //I read about get ops but if I used it you would make me implement it
+        if (strcmp(argv[*i], "-n") == 0) {
+            (*i)++; //yes before fixing I did *i++, yes I discovered the precedence of ++
+            break;
+        }
+        else
+            return 0;
+            
+            
+        
+    }
 }
 
 int main(int argc, char **argv)
 {
-    for(int i = 1; i < argc; i++) {
+    int i = 1;
+    int no_new_line = 0;
+    if(argc > 1 && argv[1][0] == '-')
+        no_new_line = check_flags(argc,argv, &i);
+    //printf("debug i %d\n",i);
+    for(i; i < argc; i++) {
         printf("%s ", argv[i]);
     }
-    printf("\n");
+    if(!no_new_line)
+        printf("\n");
 
 }
