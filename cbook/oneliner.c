9847b7db6758987691302831d5220ffc35f268b9
Nicolo <nicolovizzini2001@gmail.com>
2025-12-09 20:21:33 +0100

    should be done

--- cbook/oneliner.c
+++ cbook/oneliner.c
@@ -2,20 +2,37 @@
 
 int main()
 {
-    int c = 0;
-    int space = 0;
+    int c;
+    int space = 0, first_word = 0, first_character = 0;
     while ((c = getchar()) != EOF)
     {
+        if (c == '\n')
+        {
+            putchar(c);
+            first_character = 0;
+            first_word = 0;
+            continue;
+        }
         if (c == ' ')
         {
-            space += 1;        
+            space = 1;
         }
-        else 
-            space = 0;
-        if (space == 0)
+
+        else
+        {
+
+            if (space == 1)
+            {
+                if (first_word++)
+                    putchar('\n');
+                space = 0;
+            }
+            else if (space == 0 && first_character == 0)
+            {
+                first_word = 1;
+            }
+
             putchar(c);
-        if (space == 1)
-            putchar('\n');
-        
+        }
     }
 }
